9f4184514ce0161e2a66242f0ea675e3d883b878
lihanyu <1328411791@qq.com>
2024-02-20 02:02:33 +0800

    init

---
+++ src/main/java/org/talang/wabackend/WaBackendApplication.java
@@ -0,0 +1,13 @@
+package org.talang.wabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/talang/wabackend/WaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.talang.wabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
