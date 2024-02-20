2f2b09abbd7708ca1b584025104a5627a6a33f6d
lihanyu <1328411791@qq.com>
2024-02-21 00:52:33 +0800

    新增测试点

---
+++ backend/src/test/java/org/talang/wabackend/sdk/Text2ImageTest.java
@@ -0,0 +1,35 @@
+package org.talang.wabackend.sdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.talang.sdk.SdWebui;
+import org.talang.sdk.models.options.Txt2ImageOptions;
+import org.talang.sdk.models.results.Txt2ImgResult;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
+
+@SpringBootTest
+public class Text2ImageTest {
+
+    @Autowired
+    private SdWebui sdWebui;
+
+    @Test
+    public void text2ImageTest() throws IOException {
+        Txt2ImgResult txt2ImgResult = sdWebui.txt2Img(Txt2ImageOptions.builder()
+                .prompt("1dog")
+                .samplerName("DPM++ 2M Karras")
+                .steps(20)
+                .cfgScale(7)
+                .seed(32749528)
+                .build());
+
+        Path step1Path = Paths.get("docs/images/txt2img-dog.png");
+        Files.write(step1Path, Base64.getDecoder().decode(txt2ImgResult.getImages().get(0)));
+    }
+}
