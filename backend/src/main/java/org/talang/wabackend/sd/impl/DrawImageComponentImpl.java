496a45f528905627fcdacdcf33c8369fab9e72d2
ChenNing <1915664627@qq.com>
2024-03-03 07:23:36 +0800

    修复画图完成后参数压入错误

--- backend/src/main/java/org/talang/wabackend/sd/impl/DrawImageComponentImpl.java
+++ backend/src/main/java/org/talang/wabackend/sd/impl/DrawImageComponentImpl.java
@@ -61,9 +61,8 @@ public class DrawImageComponentImpl implements DrawImageComponent {
 
             String imageId = imageComponent.saveImage(decode, userId);
             sdImageService.saveSdImage(imageId,txt2ImgResult,userId);
-            String imageParams = JSONUtil.toJsonStr(txt2ImgResult.getParameters());
 
-            taskService.setFinishDrawStatus(taskId, imageId, imageParams);
+            taskService.setFinishDrawStatus(taskId, imageId, "");
 
             sdDrawFinshHandle.drawFinishHandle(taskId);
         } catch (Exception e) {
