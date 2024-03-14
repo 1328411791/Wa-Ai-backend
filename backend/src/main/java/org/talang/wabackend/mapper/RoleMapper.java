96d685b6ac6410876bf0bf62f28874c9f78f4fe4
lihanyu <1328411791@qq.com>
2024-03-14 13:15:31 +0800

    feat: 权限

---
+++ backend/src/main/java/org/talang/wabackend/mapper/RoleMapper.java
@@ -0,0 +1,18 @@
+package org.talang.wabackend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.talang.wabackend.model.generator.Role;
+
+/**
+ * @author lihanyu
+ * @description 针对表【sd_role】的数据库操作Mapper
+ * @createDate 2024-03-14 13:11:53
+ * @Entity org.talang.wabackend.model.generator.Role
+ */
+public interface RoleMapper extends BaseMapper<Role> {
+
+}
+
+
+
+
---
+++ backend/src/main/java/org/talang/wabackend/model/generator/Role.java
@@ -0,0 +1,47 @@
+package org.talang.wabackend.model.generator;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @TableName sd_role
+ */
+@TableName(value = "sd_role")
+@Data
+public class Role implements Serializable {
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+    /**
+     *
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+    /**
+     *
+     */
+    @TableField(value = "role_name")
+    private Integer roleName;
+    /**
+     *
+     */
+    @TableField(value = "description")
+    private Integer description;
+    /**
+     *
+     */
+    @TableField(value = "is_delete")
+    private Integer isDelete;
+    /**
+     *
+     */
+    @TableField(value = "update_time", fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+    /**
+     *
+     */
+    @TableField(value = "create_time", fill = FieldFill.INSERT)
+    private Date createTime;
+}
